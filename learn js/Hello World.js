3c22e090777b622175c187b09364dc0cc629e5c4
Alen Pontejos <alenpontejos@gmail.com>
2020-06-01 06:57:13 -0700

    made a new console log

--- learn js/Hello World.js
+++ learn js/Hello World.js
@@ -3,3 +3,5 @@ console.log('hello world, my name is micky mouse');
 console.log('welcome to Canada');
 
 console.log("i like playing basketball");
+
+console.log('a change made in Alen\'s  branch');
