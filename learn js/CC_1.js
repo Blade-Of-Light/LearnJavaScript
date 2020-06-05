2e00ff8fb36fe7e9559770df60f6988b3200e27b
Alen Pontejos <alenpontejos@gmail.com>
2020-06-05 07:01:16 -0700

    bmi comparison with function

---
+++ learn js/CC_1.js
@@ -0,0 +1,25 @@
+var mark = {
+    name : 'mark',
+    mass : 2,
+    height : 3
+}
+
+var john = {
+    name : 'john',
+    mass : 1,
+    height : 4
+}
+
+//per = person. For comparing total bmi
+function bool (per1, per2) {
+ bmi1 = per1.mass * Math.pow(per1.height, 2);
+ bmi2 = per2.mass * Math.pow(per2.height, 2);
+
+    console.log (bmi1 + ' ' + bmi2);
+    if (bmi1 ===  bmi2){
+        return console.log('their bmi are the same at: ' + bmi1 );
+    } else if  (bmi1 > bmi2){ 
+        return console.log(per1.name + '\'s bmi is higher at: ' + bmi1);
+    }
+    return console.log(per2.name + '\'s bmi is higher at: ' + bmi2);
+}
