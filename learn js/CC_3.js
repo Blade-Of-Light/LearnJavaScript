211806e29ad1211bef445521e68e9afe1ba88fd4
Alen Pontejos <alenpontejos@gmail.com>
2020-06-06 14:54:54 -0700

    Made function with function call

---
+++ learn js/CC_3.js
@@ -0,0 +1,23 @@
+//input an array
+function tipAmmount (arr){
+    var arr = arr;
+    var tip = [];
+    var tipTotal = []
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] < 50){
+            tipTotal[i] = arr[i] * 0.2 + arr[i];
+            tip[i] = arr[i] * 0.2;
+            continue
+        } else if (arr[i] < 200) {
+            tipTotal[i] = arr[i] * 0.15 + arr[i];
+            tip[i] = arr[i] * 0.15;
+            continue
+        }
+        tipTotal[i] = arr[i] * 0.1 + arr[i];
+        tip[i] = arr[i] * 0.1;
+    }
+    return tipTotal + ' indevidual tips are: ' + tip ;
+}
+
+console.log('printing out tip ammounts')
+console.log (tipAmmount ([124, 48, 268]));
