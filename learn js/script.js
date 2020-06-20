b6579d73fa75be28a4a1a285ac4b03c2e1f6146f
Alen Pontejos <alenpontejos@gmail.com>
2020-06-20 16:36:29 -0700

    added and changed interview question

--- learn js/script.js
+++ learn js/script.js
@@ -21,3 +21,33 @@ function retirement(retirementAge) {
         console.log((retirementAge - age) + a)
     }
 }
+
+/**
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+           console.log (name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher'){
+        return function(name) {
+            console.log(name + ", what subject do you teach?");
+        }
+     } else {
+         return function(name, test) {
+             console.log ('Hello, ' + name + ', what do you do' + test);
+         }
+     }
+}
+ */
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log (name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log(name + ", what subject do you teach?");
+        } else {
+            console.log ('Hello, ' + name + ', what do you do' + test);
+        }
+    }
+}
