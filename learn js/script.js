e86b4c980f5bf7745264bdcb094bc6688fdbf489
Alen Pontejos <alenpontejos@gmail.com>
2020-06-19 20:12:41 -0700

    different constructors

---
+++ learn js/script.js
@@ -0,0 +1,16 @@
+var personProto = {
+    calculateAge: function (){
+        console.log(2016 - this.yearOfBirth)
+    }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name : {value: 'Jane'},
+    yearOfBirth: {value: 1969},
+    job: {value: 'designer'}
+});
