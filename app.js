0f1557c4d468ce28e8e03ff6a4a3fec5b40de77c
Alen Pontejos <alenpontejos@gmail.com>
2020-06-22 16:04:45 -0700

    part 2 completed

--- app.js
+++ app.js
@@ -11,19 +11,61 @@
     Question.prototype.displayQuestion = function() {
         console.log(this.desc);
 
-        for (var i = 0; i < this.option.length; i++){
+        for (var i = 0; i < this.option.length; i++) {
             console.log(i + ' : ' + this.option[i]);
         }
     }
     
-    Question.prototype.checkAns = function(answer) {
+    Question.prototype.checkAns = function(answer, callback) {
+        var score;
+
         if (answer === this.answer){
             console.log('correct');
+            score = callback(true);
+
         } else {
             console.log('incorrect');
+            score = callback(false);
+        }
+
+        this.displayScore(score);
+    }
+
+    Question.prototype.displayScore = function (score) {
+        console.log('our current score is: ' + score + '\n' 
+        + '--------------------------')
+    }
+
+    function nextQuestion () {
+    
+        var index = Math.floor(Math.random() * qList.length);
+
+        qList[index].displayQuestion();     
+        
+        var answer = prompt('please select the correct answer');
+        
+        if (answer !== 'exit') {
+            
+            qList[index].checkAns(parseInt(answer), 
+                keepScore);
+            
+            nextQuestion();
+
+        }
+    }
+
+    function score() {
+        var score = 0;
+        return function(correct) {
+            if(correct) {
+                score++;
+            }
+            return score;
         }
     }
     
+    var keepScore = score();
+
     //vars
 
     var q1 = new Question ('question 1, first answer is correct', ['first answer', 'second answer'], 0);
@@ -31,17 +73,11 @@
     var q2 = new Question ('question 2 third answer is correct', ['first answer', 'second answer', 'third answer'], 2);
     
     var q3 = new Question ('question 3 answer 3 is correct', ['answer 1', 'answer 2', 'answer 3', 'answer 4'], 2);
-    
+
     var qList = [q1, q2, q3];
-
+    
     //code
-    
-    var index = Math.floor(Math.random() * qList.length);
 
-    qList[index].displayQuestion();     
-    
-    var answer = parseInt(prompt('please select the correct answer'));
-
-    qList[index].checkAns(answer);
+    nextQuestion();
 
 })();
