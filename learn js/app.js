1cd33b5c70283ca26e8dc924eef652116299abb4
Alen Pontejos <alenpontejos@gmail.com>
2020-06-24 16:08:52 -0700

    latest version before bug

--- learn js/app.js
+++ learn js/app.js
@@ -11,13 +11,24 @@ var budgetController = (function() {
 //UI CONTROLLER
 var UIController = (function () {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
     return {
         getInput: function() {
             return {
-                type : document.querySelector('.add__type').value,
-                description : document.querySelector('add__description').value,
-                value : document.querySelector('add__value').value
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
             }
+        },
+
+        getDOMstrings : function() {
+            return DOMstrings;
         }
     };
 
@@ -28,6 +39,20 @@ var UIController = (function () {
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+
+        });
+    };
 
     
     var ctrlAddItem = function() {
@@ -47,15 +72,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-
-    });
-
+    }
 
 })(budgetController, UIController);
+
+
+controller.init();
