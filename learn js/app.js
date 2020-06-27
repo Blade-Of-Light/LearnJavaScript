a4482356946ff2ef6e9d5e8595c07557861e1bd9
Alen Pontejos <alenpontejos@gmail.com>
2020-06-27 14:15:09 -0700

    framework for data structure added

--- learn js/app.js
+++ learn js/app.js
@@ -1,7 +1,34 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
 
-    //some code
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+
+    var allExpenses = [];
+    var allIncomes = [];
+    var totalExpenses = 0;
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    
+    }
 
 })();
 
