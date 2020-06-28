c1b8e0ebc23fc112af0afb654bb2b144b5d034cb
Alen Pontejos <alenpontejos@gmail.com>
2020-06-27 20:48:10 -0700

    added UI functionallity

--- learn js/app.js
+++ learn js/app.js
@@ -70,7 +70,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -80,6 +82,28 @@ var UIController = (function () {
                 description : document.querySelector(DOMstrings.inputDescription).value,
                 value : document.querySelector(DOMstrings.inputValue).value
             }
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            
+            //TODO make html string with place holder text
+            if(type === 'inc'){
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if(type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            //TODO replace place holder tetxt
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //TOTO insert into  dom
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
         },
 
         getDOMstrings : function() {
@@ -115,9 +139,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 1. Get the field input data
             input = UICtrl.getInput();
+            
             // 2. Add the item to the budget controller
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
             // 3. Add item to the UI
+            UICtrl.addListItem(newItem, input.type);
             
             // 4. Calculate the buget
             
