c83c15b70db3a951f17a3a8e82a01566d85b2ae5
Alen Pontejos <alenpontejos@gmail.com>
2020-06-28 15:57:05 -0700

    update UI, 6:32,

--- learn js/app.js
+++ learn js/app.js
@@ -105,8 +105,14 @@ var UIController = (function () {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
+
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage' 
     }
 
     return {
@@ -154,6 +160,14 @@ var UIController = (function () {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            
+        },
+
         getDOMstrings : function() {
             return DOMstrings;
         }
@@ -189,7 +203,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
         
         // 3. Display the budget to the UI
-        console.log(budget);    
+        UICtrl.displayBudget(budget);    
     }
     
     var ctrlAddItem = function() {
